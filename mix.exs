6f4ae2a060b85fdb009cf5175980337f52add648
Carlo Colombo <carlo.colombo@gmx.com>
2016-05-28 21:52:14 +0100

    added mock dependency

--- mix.exs
+++ mix.exs
@@ -46,6 +46,7 @@ defmodule Meter.Mixfile do
     [{:httpoison, "~> 0.8"},
      {:earmark, "~> 0.2", only: :dev},
      {:ex_doc, "~> 0.11", only: :dev},
+     {:mock, "~> 0.1.1", only: :test},
      {:credo, "~> 0.3", only: [:test, :dev]}
     ]
   end
