0df1e1a1f78b87b9c7350e56d3ef4d779b8d360f
Carlo Colombo <carlo.colombo@gmx.com>
2016-05-28 21:57:32 +0100

    macro to set and unset application wide configuration

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,31 @@
-ExUnit.start()
+ExUnit.start([
+  assert_receive_timeout: 500,
+  refute_receive_timeout: 500
+])
+
+defmodule TestHelper do
+
+  @doc """
+  Add configuration to the envs and after executing the code remove it from the configuration
+  """
+  defmacro with_env(confs, do: body) do
+    configs = for {app, envs} <- confs do
+      for {key, value} <- envs do
+        {app, key, value}
+      end
+    end
+    |> List.flatten
+    |> Enum.map(fn t -> Tuple.to_list(t) end)
+
+    quote do
+      unquote(configs)
+      |> Enum.map(fn args -> apply(Application, :put_env, args) end)
+
+      unquote(body)
+
+      unquote(configs)
+      |> Enum.map(fn args -> apply(Application, :delete_env, Enum.slice(args, 0..1)) end)
+    end
+
+  end
+end
