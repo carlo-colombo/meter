705d55f71e25958b219e3a87d33009651d7f9083
Carlo Colombo <carlo.colombo@gmx.com>
2016-05-28 08:02:09 +0100

    param_generator and test

--- lib/utils.ex
+++ lib/utils.ex
@@ -7,6 +7,9 @@ defmodule Meter.Utils do
   iex> Meter.Utils.custom_dimensions([], [key: "value"])
   []
 
+  iex> Meter.Utils.custom_dimensions([:key], [])
+  []
+
   iex> Meter.Utils.custom_dimensions([:key], [key: "value"])
   [{"cd1", "value"}]
 
@@ -19,10 +22,62 @@ defmodule Meter.Utils do
 ```
   """
   @spec custom_dimensions(list(atom), Keyword.t) :: Keyword.t
+  def custom_dimensions([], _), do: []
+  def custom_dimensions(_, []), do: []
   def custom_dimensions(dimensions, kwargs) do
     1..length(dimensions)
     |> Enum.map(fn i -> "cd#{i}" end)
     |> Enum.zip(dimensions)
     |> Enum.map(fn {cdi,argname} -> {cdi, kwargs[argname]} end )
+  end
+
+  @doc """
+  ```
+    iex>Meter.Utils.param_generator(:fn, [arg1: 3, arg2: 5], "UA-123", [cid: :arg2])
+    [v: 1,
+     tid: "UA-123",
+     cid: 5,
+     ds: "server",
+     t: "pageview",
+     dt: "fn",
+     dp: "/fn"] ++
+    [{"cd1", 5},{"cd2", 3}]
+
+    iex>Meter.Utils.param_generator(:fn, [])
+    [v: 1,
+     tid: nil,
+     cid: nil,
+     ds: "server",
+     t: "pageview",
+     dt: "fn",
+     dp: "/fn"]
+
+    iex>Meter.Utils.param_generator(:fn, [], "UA-123")
+    [v: 1,
+     tid: "UA-123",
+     cid: nil,
+     ds: "server",
+     t: "pageview",
+     dt: "fn",
+     dp: "/fn"]
+
+  ```
+  """
+  def param_generator(function_name, kwargs, tid, mapping) do
+      [v: 1,
+       tid: tid,
+       cid: kwargs[mapping[:cid]],
+       ds: "server",
+       t: "pageview",
+       dt: "#{function_name}",
+       dp: "/#{function_name}"
+      ] ++
+      custom_dimensions([:arg2, :arg1], kwargs)
+  end
+  def param_generator(function_name, kwargs) do
+    param_generator(function_name, kwargs, nil, nil)
+  end
+  def param_generator(function_name, kwargs, tid) do
+    param_generator(function_name, kwargs, tid, nil)
   end
 end
